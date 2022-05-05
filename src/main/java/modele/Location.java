c51e800224090570b1b90c2b2e30bfc2cc7f5579
Émilie Vey <nath.v26@gmail.com>
2022-05-05 11:29:51 +0200

    Location class

---
+++ src/main/java/modele/Location.java
@@ -0,0 +1,56 @@
+package modele;
+
+import java.util.Random;
+
+public class Location {
+
+    private final int row;
+    private final int col;
+
+    public Location(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public static Location generateRandomLocation(int bound) {
+        Random rdm = Jeu.RANDOM;
+        int x = rdm.nextInt(bound);
+        int y = rdm.nextInt(bound);
+
+        return new Location(x, y);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + col;
+        result = prime * result + row;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Location other = (Location) obj;
+        if (col != other.col)
+            return false;
+        if (row != other.row)
+            return false;
+        return true;
+    }
+
+}
