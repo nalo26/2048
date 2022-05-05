a0ec3874e243566f6a590a67dd668ae7db7390d9
Émilie Vey <nath.v26@gmail.com>
2022-05-05 11:29:51 +0200

    Location class

--- src/main/java/modele/Direction.java
+++ src/main/java/modele/Direction.java
@@ -1,5 +1,15 @@
 package modele;
 
 public enum Direction {
-    haut, bas, gauche, droite
+    UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGHT(1, 0);
+
+    private final Location location;
+
+    private Direction(int x, int y) {
+        this.location = new Location(x, y);
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
 }
