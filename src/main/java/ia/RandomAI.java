a2e0a6e3ba69338379f874f96e64ce8c2ebd76d9
Émilie Vey <nath.v26@gmail.com>
2022-05-20 12:36:57 +0200

    Random AI

--- src/main/java/ia/RandomAI.java
+++ src/main/java/ia/RandomAI.java
@@ -1,5 +1,18 @@
 package ia;
 
-public class RandomAI {
+import modele.Direction;
+import modele.Game;
+
+public class RandomAI implements AI {
     
+    private Game game;
+
+    public RandomAI(Game game) {
+        this.game = game;
+    }
+
+    public void play() {
+        int random = Game.RANDOM.nextInt(4);
+        this.game.move(Direction.values()[random]);
+    }
 }
