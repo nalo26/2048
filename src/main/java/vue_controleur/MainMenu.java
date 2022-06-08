9668faf5792842edceda0d43f09b5a118248d528
Galding <nathan.peyronnet@gmail.com>
2022-06-08 16:18:13 +0200

    add title

--- src/main/java/vue_controleur/MainMenu.java
+++ src/main/java/vue_controleur/MainMenu.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.NORTH;
 import static vue_controleur.Swing2048.PIXEL_PER_SQUARE;
 
 public class MainMenu extends JPanel {
@@ -16,9 +17,16 @@ public class MainMenu extends JPanel {
 
     public MainMenu() {
         game = Game.init(4);
-        //setSize(new Dimension(game.getSize() * PIXEL_PER_SQUARE, (int) ((game.getSize() + 0.5) * PIXEL_PER_SQUARE)));
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(game.getSize() * PIXEL_PER_SQUARE, (int) ((game.getSize() + 0.5) * PIXEL_PER_SQUARE)));
+
+        JLabel title = new JLabel("2048 GAME");
+        title.setFont(new Font("serif", Font.BOLD, 100));
+        title.setForeground(Color.RED);
+        title.setVerticalAlignment(JLabel.NORTH);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        add(title, NORTH);
+
         JLabel baseGameClickableLabel = new JLabel("Base Game");
         baseGameClickableLabel.addMouseListener(new MouseAdapter() {
             @Override
@@ -27,15 +35,19 @@ public class MainMenu extends JPanel {
                 Swing2048 vue = new Swing2048(game);
                 game.addObserver(vue);
                 vue.setVisible(true);
-                remove(baseGameClickableLabel);
+                removeAll();
                 vue.setFocusable(true);
                 setFocusable(false);
-                //vue.addKeyListener(vue.addKeyListener());
                 add(vue);
                 vue.requestFocusInWindow();
                 updateUI();
             }
         });
+        baseGameClickableLabel.setHorizontalAlignment(JLabel.CENTER);
+        baseGameClickableLabel.setVerticalAlignment(JLabel.CENTER);
+        baseGameClickableLabel.setFont(new Font("serif", Font.PLAIN, 32));
         add(baseGameClickableLabel, CENTER);
+
+
     }
 }
